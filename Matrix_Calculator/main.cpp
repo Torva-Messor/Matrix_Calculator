54cd3a94d114a36075ccadd784bd3fb9558ea594
UinclisWin <uinclis@outlook.de>
2020-10-05 16:28:38 +0200

    Errors

--- Matrix_Calculator/main.cpp
+++ Matrix_Calculator/main.cpp
@@ -6,26 +6,74 @@
 using namespace std;
 
 
+void matrixMultiplicationInstructions() {
+
+	cout << "The multiplication of two matrices is only possible if the matrice A row quantity and the matrice B coloumn quantity are the same." << endl;
+	cout << "Ex:" << endl;
+
+	float fRowsMatrixA = 2, fColumnsMatrixA = 3;
+	int iAMatrixEX[] = { 1, 2, 3 };
+	int iBMatrixEX[] = { 4, 5, 6 };
+	int* iMatrixEX[] = { iAMatrixEX, iBMatrixEX };
+
+	for (int i = 0; i < fRowsMatrixA; i++)
+	{
+		for (int j = 0; j < fColumnsMatrixA; j++) {
+			cout << iMatrixEX[i][j] << "  ";
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+	cout << "This is a " << fRowsMatrixA << " x [" << fColumnsMatrixA << "] matrice." << endl << endl;
+
+	int iRowsMatrixB = 3, iColumnsMatrixB = 3;
+	int iAMatrixBEX[] = { 6, 5, 4 };
+	int iBMatrixBEX[] = { 1, 2, 3 };
+	int iCMatrixBEX[] = { 2, 4, 6 };
+	int* iMatrixBEX[] = { iAMatrixBEX, iBMatrixBEX };
+
+	for (int i = 0; i < iRowsMatrixB; i++)
+	{
+		for (int j = 0; j < iColumnsMatrixB; j++) {
+			cout << iMatrixBEX[i][j] << "  ";
+		}
+		cout << endl;
+	}
+
+	cout << endl << endl;
+	cout << "This is a [" << iRowsMatrixB << "] x " << iColumnsMatrixB << " matrice." << endl << endl << endl;
+	cout << "As you can see. The matrices A (2x[3]) and B ([3]x3)" << "can be multiplificated." << endl;
+	cout << "Press ENTER to continue you multiplifications of matrices." << endl;
+	cin.get();
+
+}
+
+
 double ** matrixMultiplication() {
 	
 	//Pointers
 	double* dPointerdRowsMatrixA = NULL;
-	double* dPointerdColumnMatrixA = NULL;
+	//double* dPointerdColumnMatrixA = NULL;
 	double* dPointerdRowsMatrixB = NULL;
-	double* dPointerdColumnMatrixB = NULL;
+	//double* dPointerdColumnMatrixB = NULL;
 
 	//user inputs
 	double dRowsMatrixA, dColumnMatrixA;
 	double dRowsMatrixB, dColumnMatrixB;
 
-	//store memorie
+	//store memory pointers
 	dPointerdRowsMatrixA = new double[dRowsMatrixA];
-	dPointerdColumnMatrixA = new double[dColumnMatrixA];
+	//dPointerdColumnMatrixA = new double[dColumnMatrixA];
 	dPointerdRowsMatrixB = new double[dRowsMatrixB];
-	dPointerdColumnMatrixB = new double[dColumnMatrixB];
+	//dPointerdColumnMatrixB = new double[dColumnMatrixB];
 	
 	//Temp variables
-	double tempdRowsMatrixA, tempdColumnMatrixA, tempdRowsMatrixB, tempdColumnMatrixB;
+	//Maybe I can change it with: double sLen = sizeof(dPointerdRowsMatrixA)/sizeof(dPointerdRowsMatrixA[0])  
+	double dTempdRowsMatrixA[10]; 
+	//double dTempdColumnMatrixA[10]; 
+	double dTempdRowsMatrixB[10]; 
+	//double dTempdColumnMatrixB[10];
 
 	//float fRowsMatrixAValuers[fRowsMatrixA], fColumnMatrixAValuers[fColumnMatrixA];
 	//float fRowsMatrixBValuers[fRowsMatrixB], fColumnMatrixBValuers[fColumnMatrixB];
@@ -51,13 +99,21 @@ double ** matrixMultiplication() {
 	cout << "4 5 6	 [ENTER]" << endl;
 	cout << "7 8 9	 [ENTER]" << endl << endl;
 	cout << "Now your turn: " << endl;
-	for (int i = 0; i < length; i++)
+	for (int i = 0; i < dRowsMatrixA; i++)
 	{
-
+		cout << "Enter the itens for the line nummber " << i + 1 << endl;
+		cin >> dTempdRowsMatrixA[i];
+		*(dPointerdRowsMatrixA + i) = dTempdRowsMatrixA[i];
 	}
 
+	for (int i = 0; i < dRowsMatrixA; i++)
+	{
+		cout << " " << *(dPointerdRowsMatrixA + i)<< " " << endl;
 
-
+	}
+	
+	
+	
 	cout << "Put here how many rows there is in the secound matrix (Matrix B):  ";
 	cin >> dRowsMatrixB;
 	cout << endl;
@@ -86,49 +142,6 @@ double ** matrixSum() {
 }
 
 double ** matrixSubtraction() {
-
-}
-
-void matrixMultiplicationInstructions() {
-
-	cout << "The multiplication of two matrices is only possible if the matrice A row quantity and the matrice B coloumn quantity are the same." << endl;
-	cout << "Ex:" << endl;
-
-	float fRowsMatrixA = 2, fColumnsMatrixA = 3;
-	int iAMatrixEX[] = {1, 2, 3};
-	int iBMatrixEX[] = {4, 5, 6};
-	int* iMatrixEX[] = { iAMatrixEX, iBMatrixEX };
-
-	for (int i = 0; i < fRowsMatrixA; i++)
-	{
-		for (int j = 0; j < fColumnsMatrixA; j++) {
-			cout << iMatrixEX[i][j] << "  ";
-		}
-		cout << endl;
-	}
-	
-	cout << endl << endl;
-	cout << "This is a " << fRowsMatrixA << " x [" << fColumnsMatrixA << "] matrice." << endl << endl;
-
-	int iRowsMatrixB = 3, iColumnsMatrixB = 3;
-	int iAMatrixBEX[] = { 6, 5, 4 };
-	int iBMatrixBEX[] = { 1, 2, 3 };
-	int iCMatrixBEX[] = { 2, 4, 6 };
-	int* iMatrixBEX[] = { iAMatrixBEX, iBMatrixBEX };
-
-	for (int i = 0; i < iRowsMatrixB; i++)
-	{
-		for (int j = 0; j < iColumnsMatrixB; j++) {
-			cout << iMatrixBEX[i][j] << "  ";
-		}
-		cout << endl;
-	}
-
-	cout << endl << endl;
-	cout << "This is a [" << iRowsMatrixB << "] x " << iColumnsMatrixB << " matrice." << endl << endl << endl;
-	cout << "As you can see. The matrices A (2x[3]) and B ([3]x3)" << "can be multiplificated." << endl;
-	cout << "Press ENTER to continue you multiplifications of matrices." << endl;
-	cin.get();
 
 }
 
