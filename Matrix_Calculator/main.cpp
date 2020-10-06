897a1284bc714f79fda099e07967abcf17bab2fd
UinclisWin <uinclis@outlook.de>
2020-10-06 13:20:53 +0200

    Matrix Multiplication finished (maybe)

--- Matrix_Calculator/main.cpp
+++ Matrix_Calculator/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <windows.h>
+
+
 
 
 using namespace std;
@@ -50,98 +53,123 @@ void matrixMultiplicationInstructions() {
 }
 
 
-double ** matrixMultiplication() {
-	
-	//Pointers
-	double* dPointerdRowsMatrixA = NULL;
-	//double* dPointerdColumnMatrixA = NULL;
-	double* dPointerdRowsMatrixB = NULL;
-	//double* dPointerdColumnMatrixB = NULL;
+double** matrixMultiplication() {
 
-	//user inputs
-	double dRowsMatrixA, dColumnMatrixA;
-	double dRowsMatrixB, dColumnMatrixB;
 
-	//store memory pointers
-	dPointerdRowsMatrixA = new double[dRowsMatrixA];
-	//dPointerdColumnMatrixA = new double[dColumnMatrixA];
-	dPointerdRowsMatrixB = new double[dRowsMatrixB];
-	//dPointerdColumnMatrixB = new double[dColumnMatrixB];
-	
-	//Temp variables
-	//Maybe I can change it with: double sLen = sizeof(dPointerdRowsMatrixA)/sizeof(dPointerdRowsMatrixA[0])  
-	double dTempdRowsMatrixA[10]; 
-	//double dTempdColumnMatrixA[10]; 
-	double dTempdRowsMatrixB[10]; 
-	//double dTempdColumnMatrixB[10];
-
-	//float fRowsMatrixAValuers[fRowsMatrixA], fColumnMatrixAValuers[fColumnMatrixA];
-	//float fRowsMatrixBValuers[fRowsMatrixB], fColumnMatrixBValuers[fColumnMatrixB];
-
-	char cInstructions;
-	cout << "Do you want to see the instructions for a multiplications of matrices? (y/n)" << endl;
-	
-	cin >> cInstructions;
-	if (cInstructions == 'y')
-	{
-		matrixMultiplicationInstructions();
-	}
-
-	cout << "Put down  the nescessary valouers..." << endl;
-	cout << "Put here how many rows there is in the first matrix (Matrix A):  ";
-	cin >> dRowsMatrixA;
-	cout << endl;
-	cout << "Put here how many columns there is in the matrix A:  ";
-	cin >> dColumnMatrixA;
-	cout << endl << endl;
-	cout << "Put here all numbers of Matrice A" << endl << "Ex: " << endl;
-	cout << "1 2 3	 [ENTER]" << endl;
-	cout << "4 5 6	 [ENTER]" << endl;
-	cout << "7 8 9	 [ENTER]" << endl << endl;
-	cout << "Now your turn: " << endl;
-	for (int i = 0; i < dRowsMatrixA; i++)
-	{
-		cout << "Enter the itens for the line nummber " << i + 1 << endl;
-		cin >> dTempdRowsMatrixA[i];
-		*(dPointerdRowsMatrixA + i) = dTempdRowsMatrixA[i];
-	}
-
-	for (int i = 0; i < dRowsMatrixA; i++)
-	{
-		cout << " " << *(dPointerdRowsMatrixA + i)<< " " << endl;
-
-	}
-	
-	
-	
-	cout << "Put here how many rows there is in the secound matrix (Matrix B):  ";
-	cin >> dRowsMatrixB;
-	cout << endl;
-	cout << "Put here how many columns there is in the matrix B:  ";
-	cin >> dColumnMatrixB;
-	cout << endl << endl;
-
-	if (dColumnMatrixA != dRowsMatrixB)
+	char cMatrixCorrect;
+	while (cMatrixCorrect == 'y')
 	{
 
-		cout << "I'm sorry but this matrices can't be multiplicated" << endl;
 
-	}
-	else
-	{
-		cout << "Please put the numbers of the first row (first line) of matrice A. Separeted by a SPACE between them" << endl; 
-		for (int i = 0; i < dRowsMatrixA; i++)
+
+		double dMatrixA[15][15], dMatrixB[15][15], dMultMatrixAB[15][15];
+		int iRowsMatrixA, iColumnMatrixA, iRowsMatrixB, iColumnMatrixB, iTempMultMatrixAB;
+		
+
+		char cInstructions;
+		cout << "Do you want to see the instructions for a multiplications of matrices? (y/n)" << endl;
+
+		cin >> cInstructions;
+		if (cInstructions == 'y')
+		{
+			matrixMultiplicationInstructions();
+		}
+
+
+		cout << "Put down  the nescessary valouers..." << endl;
+		cout << "Put here how many rows there is in the first matrix (Matrix A):  ";
+		cin >> iRowsMatrixA;
+		cout << endl;
+		cout << "Put here how many columns there is in the matrix A:  ";
+		cin >> iColumnMatrixA;
+		cout << endl << endl;
+		cout << "Put here all numbers of Matrice A" << endl << "Ex: " << endl;
+		cout << "1 2 3	 [ENTER]" << endl;
+		cout << "4 5 6	 [ENTER]" << endl;
+		cout << "7 8 9	 [ENTER]" << endl << endl << endl;
+		cout << "Now it's your turn: " << endl;
+
+		for (int i = 0; i < iRowsMatrixA; i++)
+		{
+			for (int j = 0; j < iColumnMatrixA; j++) {
+				cin >> dMatrixA[i][j];
+			}
+		}
+
+		cout << endl << endl;
+		cout << "Your matrice looks like this" << endl;
+		for (int i = 0; i < iRowsMatrixA; i++)
+		{
+			for (int j = 0; j < iColumnMatrixA; j++) {
+				cout << dMatrixA[i][j];
+			}
+			cout << endl;
+		}
+
+
+
+		cout << "Put here how many rows there is in the secound matrix (Matrix B):  ";
+		cin >> iRowsMatrixB;
+		cout << endl;
+		cout << "Put here how many columns there is in the matrix B:  ";
+		cin >> iColumnMatrixB;
+		cout << endl << endl;
+
+		for (int i = 0; i < iRowsMatrixB; i++)
+		{
+			for (int j = 0; j < iColumnMatrixB; j++) {
+				cin >> dMatrixB[i][j];
+			}
+		}
+
+		cout << endl << endl;
+		cout << "Your matrice looks like this" << endl;
+		for (int i = 0; i < iRowsMatrixB; i++)
+		{
+			for (int j = 0; j < iColumnMatrixB; j++)
+			{
+				cout << dMatrixB[i][j] << " ";
+			}
+			cout << endl;
+		}
+
+		cout << endl;
+		cout << "Does your matrices looke like u wanted ?";
+		cout << endl;
+		cout << "(y/n)?";
+		cin >> cMatrixCorrect;
+
+		cout << endl << endl;
+		if (iColumnMatrixA != iRowsMatrixB)
 		{
 
+			cout << "I'm sorry but this matrices can't be multiplicated" << endl;
+
+		}
+		else
+		{
+			for (int i = 0; i < iRowsMatrixA; i++)
+			{
+				for (int j = 0; j < iRowsMatrixB; j++)
+				{
+					for (int k = 0; k < iRowsMatrixB; k++)
+					{
+						iTempMultMatrixAB += dMatrixA[i][k] * dMatrixB[k][j];
+					}
+					dMultMatrixAB[i][j] = iTempMultMatrixAB;
+					iTempMultMatrixAB = 0;
+				}
+			}
 		}
 	}
+	
 }
 
-double ** matrixSum() {
+double** matrixSum() {
 
 }
 
-double ** matrixSubtraction() {
+double** matrixSubtraction() {
 
 }
 
@@ -160,22 +188,22 @@ void instructions() {
 
 }
 
-int main(int arg, char * argv[]){
+int main(int arg, char* argv[]) {
 
-	
+
 
 	float fMatrixOperation;
 	char sInstructions;
 	cout << "Do you want to see the instructions ? (y/n)";
 	cin >> sInstructions;
-	
+
 	if (sInstructions == 'y')
 	{
 		instructions();
 	}
-	
 
-	
+
+
 	cout << "Please select the option you want to do..." << endl;
 	cout << "(1) for Multiplication of matrices" << endl;
 	cout << "(2) for sum of matrices" << endl;
@@ -205,6 +233,6 @@ int main(int arg, char * argv[]){
 		exit(4);
 	}
 
-	
+
 	return 0;
 }
